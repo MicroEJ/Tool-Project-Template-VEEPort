cfd8744844aa634e27be1c9dd02e900b0518983a
Thomas Delhoménie <thomas.delhomenie@microej.com>
2024-11-13 10:34:41 +0100

    Centralize the group and version properties

--- vee-port/extensions/image-generator/build.gradle.kts
+++ vee-port/extensions/image-generator/build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     id("com.microej.gradle.j2se-library")
 }
 
-group = "com.mycompany"
-version = "0.1.0-RC"
-
 dependencies {
     implementation("com.microej.pack.ui:ui-pack:14.0.1") {
         artifact {
