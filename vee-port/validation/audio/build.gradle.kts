30c401631d33b57f9bdf96b4d1ffe36f3e1e3ff0
Clotilde Sattler <clotilde.sattler@microej.com>
2025-02-12 12:12:29 +0100

    vee-port: validation: improve explanation for VEE Port dependency

--- vee-port/validation/audio/build.gradle.kts
+++ vee-port/validation/audio/build.gradle.kts
@@ -11,10 +11,17 @@ microej {
 }
 
 dependencies {
-    // Define the VEE Port absolute path here if you test a VEE Port already built
-    microejVee(files("[vee-port-absolute-path]"))
-    // Uncomment this line and comment the line above it this testsuite project is a part of the VEE Port project
-    // microejVee(project(":[vee-port-configuration-project]"))
+    // Use a VEE Port included in the same multi-project as this validation application
+    microejVee(project(":vee-port"))
+
+    // Use a published version of a VEE Port
+    // or a local VEE Port project with includeBuild directive
+    // microejVee("com.mycompany:vee-port:1.0.0")
+
+    // Use a built VEE Port (can be used for SDK5 compatibility).
+    // In this case, architectureUsage is determined by the VEE Port
+    // Use double "\" on Windows
+    // microejVee(files("/path/to/vee-port/build/veePort/source"))
 }
 
 testing {
